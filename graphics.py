4d654f8da92d96b891c105ed8415c6e3014d806a
hazekj <hazekj@gmail.com>
2024-06-27 17:16:36 -0600

    add draw_line method to Window class

--- graphics.py
+++ graphics.py
@@ -1,6 +1,23 @@
 from tkinter import Tk, BOTH, Canvas
 
 
+class Point:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, p1: Point, p2: Point):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas: Canvas, fill_color: str):
+        canvas.create_line(
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
+        )
+
+
 class Window:
     def __init__(self, width, height):
         self.root = Tk()
@@ -22,19 +39,5 @@ class Window:
     def close(self):
         self.running = False
 
-
-class Point:
-    def __init__(self, x: int, y: int):
-        self.x = x
-        self.y = y
-
-
-class Line:
-    def __init__(self, p1: Point, p2: Point):
-        self.p1 = p1
-        self.p2 = p2
-
-    def draw(self, canvas: Canvas, fill_color: str):
-        canvas.create_line(
-            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
-        )
+    def draw_line(self, line: Line, fill_color: str):
+        line.draw(self.canvas, fill_color)
