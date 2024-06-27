2f28082a58103dc0c17ecfb72f6a96cae8a90526
hazekj <hazekj@gmail.com>
2024-06-27 17:17:29 -0600

    create a line in canvas at specified points

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from graphics import Window
+from graphics import Window, Line, Point
 
 
 def main():
     win = Window(800, 600)
+    line = Line(Point(0, 0), Point(500, 500))
+    win.draw_line(line, "red")
     win.wait_for_close()
 
 
