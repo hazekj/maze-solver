c4070b3eb9503bb60ad27235a61c777523b317e0
hazekj <hazekj@gmail.com>
2024-07-01 10:38:52 -0600

    create cell

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from graphics import Window, Line, Point
+from cell import Cell
 
 
 def main():
     win = Window(800, 600)
     line = Line(Point(0, 0), Point(500, 500))
     win.draw_line(line, "red")
+    cell = Cell(win)
+    cell.draw(100, 100, 500, 500)
     win.wait_for_close()
 
 
